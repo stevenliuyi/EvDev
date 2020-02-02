6ee5140c4a951cfb7dd968fdd66f411949efe613
stevenliuyi <stevenliuyi@gmail.com>
2020-02-02 03:40:31 -0500

    add powerline fonts

--- scripts/install-tools-dev.sh
+++ scripts/install-tools-dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 apt-get update
-apt-get -y install git build-essential curl wget gnupg2 xz-utils openssl net-tools
+apt-get -y install git build-essential curl wget gnupg2 xz-utils openssl net-tools fonts-powerline
