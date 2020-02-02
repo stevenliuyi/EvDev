d4158af7440a479649e3d34174f2602c84e34728
stevenliuyi <stevenliuyi@gmail.com>
2020-02-02 04:36:37 -0500

    add font Fira Code

--- scripts/install-tools-dev.sh
+++ scripts/install-tools-dev.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+add-apt-repository universe
 apt-get update
-apt-get -y install git build-essential curl wget gnupg2 xz-utils openssl net-tools fonts-powerline
+apt-get -y install git build-essential curl wget gnupg2 xz-utils openssl net-tools fonts-powerline fonts-firacode
