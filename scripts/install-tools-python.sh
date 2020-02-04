4d18c7ede95a1a5746312059f3642d1e9bc1d649
stevenliuyi <stevenliuyi@gmail.com>
2020-02-04 06:39:06 -0500

    add yapf

--- scripts/install-tools-python.sh
+++ scripts/install-tools-python.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 apt-get update && apt-get install -y python3 python3-pip
-pip3 install --no-cache-dir python-language-server pylint jupyter numpy scipy matplotlib pandas
+pip3 install --no-cache-dir python-language-server pylint yapf jupyter numpy scipy matplotlib pandas
