e731206a9aa3816ab2935ba7720030997a44f2ef
stevenliuyi <stevenliuyi@gmail.com>
2020-02-07 00:41:03 -0500

    add keybindings.json

--- scripts/get-config-from-gist.sh
+++ scripts/get-config-from-gist.sh
@@ -6,4 +6,5 @@ gisturl="https://gist.githubusercontent.com/${gistid}/raw"
 mkdir -p ../snippets
 curl -o ../extensions.json "${gisturl}/extensions.json"
 curl -o ../settings.json "${gisturl}/settings.json"
+curl -o ../keybindings.json "${gisturl}/keybindings.json"
 curl -o ../snippets/latex.json "${gisturl}/snippets%257Clatex.json"
