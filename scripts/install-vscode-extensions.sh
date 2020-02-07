627c62874269898961e4e01037e7dc96cd1e907d
stevenliuyi <stevenliuyi@gmail.com>
2020-02-07 02:40:26 -0500

    install C/C++ extension from VSIX

--- scripts/install-vscode-extensions.sh
+++ scripts/install-vscode-extensions.sh
@@ -2,7 +2,18 @@
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
     echo "Installing $line using VSCode";
-    code --user-data-dir /root/.config/Code --install-extension $line
+    if [ "$line" = "ms-vscode.cpptools" ]; then
+        # Install C/C++ extension from VSIX to aviod downloading additional dependencies on extension activation
+        # Get latest version from GitHub
+        tag="$(curl --silent "https://api.github.com/repos/microsoft/vscode-cpptools/releases/latest" | jq -r .tag_name)"
+        # Download offline installer
+        echo "Downloading offline installer for ms-vscode.cpptools"
+        curl -fsSL "https://github.com/microsoft/vscode-cpptools/releases/download/${tag}/cpptools-linux.vsix" -o ./cpptools-linux.vsix
+        code --user-data-dir /root/.config/Code --install-extension ./cpptools-linux.vsix
+    else
+        code --user-data-dir /root/.config/Code --install-extension $line
+    fi
+
     # Extension installation may result in exit code 0, so check if extension
     # folder exists. The issue may be related to
     # https://github.com/microsoft/vscode/issues/80065
