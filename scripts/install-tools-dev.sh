d6528497b0617495d1a6606735995bc3983c2e81
stevenliuyi <stevenliuyi@gmail.com>
2020-02-07 17:23:21 -0500

    use fish as default shell

--- scripts/install-tools-dev.sh
+++ scripts/install-tools-dev.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-add-apt-repository universe
+apt-get -y install software-properties-common
+add-apt-repository -y universe
+add-apt-repository -y ppa:fish-shell/release-3
 apt-get update
-apt-get -y install git build-essential curl wget gnupg2 xz-utils openssl net-tools fonts-powerline fonts-firacode
+apt-get -y install git build-essential curl wget gnupg2 xz-utils openssl net-tools fonts-powerline fonts-firacode fish
+chsh -s /usr/bin/fish
