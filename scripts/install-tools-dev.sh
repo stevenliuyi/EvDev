f879e495735cebf1f5877a37a2bc106372966d7a
stevenliuyi <stevenliuyi@gmail.com>
2020-02-08 07:27:23 -0500

    add oh-my-fish

--- scripts/install-tools-dev.sh
+++ scripts/install-tools-dev.sh
@@ -5,4 +5,13 @@ add-apt-repository -y universe
 add-apt-repository -y ppa:fish-shell/release-3
 apt-get update
 apt-get -y install git build-essential curl wget gnupg2 xz-utils openssl net-tools fonts-powerline fonts-firacode fish
+
+# set fish as default shell
 chsh -s /usr/bin/fish
+
+# install oh-my-fish
+curl -L https://get.oh-my.fish > install
+fish install --noninteractive
+
+# set color theme
+fish /root/scripts/set-fish-theme.sh
